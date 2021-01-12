1af5120caaebc03c696254cdd744191bc1b69796
UlysC <ulys.chambard@gmail.com>
2021-01-12 11:16:26 +0100

    C

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5000
 const router = require('./routes.js')
 const sioux = require('./ttn.js')
 // const cors = require('cors')
