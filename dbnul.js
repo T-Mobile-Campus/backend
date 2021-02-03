a24d3ddc8d98203f085ccd458b929928150913ee
UlysC <ulys.chambard@gmail.com>
2021-02-03 16:05:04 +0100

    db fix

--- dbnul.js
+++ dbnul.js
@@ -3,45 +3,44 @@ require("dotenv").config();
 const { MongoClient } = require('mongodb');
 let mong = {};
 const uri = process.env.MONGO_URI;
-mong.client = new MongoClient(uri, { useUnifiedTopology: true });
+// mong.client = new MongoClient(uri, { useUnifiedTopology: true });
 mong.fetch = async function(clust, collec) {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
     try {
-        mong.client = new MongoClient(uri, { useUnifiedTopology: true });
-        await mong.client.connect();
-        await find( clust, collec);
+        await client.connect();
+        const cursor = await client.db(clust).collection(collec).find({});
+        const results = await cursor.toArray();
+
+        if (results.length > 0) {
+            results.forEach((result, i) => {
+                mong.results = results
+                mong.result = result;
+            });
+        } else {
+            console.log(`No data found`);
+            }
     } 
     catch (err) {
         console.error(err);
     }
     finally {
-        await mong.client.close();
+        await client.close();
     }
-    return mong.client
 }
-async function find( clust, collec) {
-    const cursor = mong.client.db(clust).collection(collec).find({});
-    const results = await cursor.toArray();
+async function find(clust, collec) {
 
-    if (results.length > 0) {
-        results.forEach((result, i) => {
-            mong.results = results
-            mong.result = result;
-        });
-    } else {
-        console.log(`No data found`);
-    }
 }
 mong.addDoc = async (clust, collec, doc) => {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
     try {
-        mong.client = new MongoClient(uri, { useUnifiedTopology: true });
-        await mong.client.connect();
-        mong.client.db(clust).collection(collec).insertOne({ doc })
+        await client.connect();
+        await client.db(clust).collection(collec).insertOne({ doc })
     }
     catch (e) {
         console.error(e)
     }
     finally {
-        await mong.client.close();
+        await client.close();
     }
 }
 module.exports = mong
