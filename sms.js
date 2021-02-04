9a9dae23b5ddb490cfb4f566ccadb7139e4c0431
UlysC <ulys.chambard@gmail.com>
2021-02-04 09:58:02 +0100

    set threshhold 7000

--- sms.js
+++ sms.js
@@ -3,7 +3,7 @@ const sioux = require('./ttn.js');
 var client = new twilio('AC6dbadb804366f1f0e80f4bec663ef9ce', '24626cd543596926a336ccfaa3d3051f');
 var arr = ["+33755446464","+33629052337", "+33695382555"];
 sioux.eventEmitter.on("update", data => {
-  if (Math.max(...sioux.vibr)> 700){
+  if (Math.max(...sioux.vibr)> 7000){
     arr.forEach(function(value){console.log(value);
 client.messages.create({
     
